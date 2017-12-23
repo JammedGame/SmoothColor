de87f3104d90c5fb140a8b2920e28bbf95b00482
Nikola <glamdring0@hotmail.com>
2017-12-23 02:12:29 +0100

    HumanGen

--- Code/HumanGen.ts
+++ Code/HumanGen.ts
@@ -9,7 +9,7 @@ class HumanGen
     private _GameScene:GameScene;
     private _Difficulty:number;    
     private _CurrLane:number;
-    private _TotalDistance:number[] = [100, 100,100];
+    private _TotalDistance:number = 960;
     private _PrevPosX:number = -1;
     private _CurrPosX:number;
   
@@ -25,20 +25,12 @@ class HumanGen
     }
     public generateParameters(difficulty:number)
     {
-        this._CurrLane = Math.random() * 2;
-        switch(difficulty)
-        {
-            case 0: 
-            {        
-                this._CurrPosX = Math.random()*(this._TotalDistance[this._CurrLane] - 20) + 20;
-                if(this._PrevPosX == this._CurrPosX)
-                {
-                    this._TotalDistance[this._CurrLane] += (this._CurrPosX - this._PrevPosX) + this._CurrPosX;
-                }
-                else
-                {
-                    this._TotalDistance[this._CurrLane] += this._CurrPosX;
-                }
+        this._CurrLane = Math.round(Math.random() * 2);
+                
+                this._CurrPosX = Math.round(Math.random()*(Math.round(600/(difficulty+1)) - 200) + 200);
+                
+                this._TotalDistance += Math.abs(this._CurrPosX - this._PrevPosX) + this._CurrPosX;
+                console.log(this._TotalDistance, this._CurrPosX, this._PrevPosX);
                 let color:Engineer.Color = this.generateColor();
                 let colorPoints:number[]=[color.R,color.G,color.B];
                 let humanPoints:number;
@@ -71,27 +63,25 @@ class HumanGen
                     humanPoints = 4;
                 }      
                 
-                new Human(this._CurrLane, color, humanPoints, this._TotalDistance[this._CurrLane], this._GameScene);         
+                
+                new Human(this._CurrLane, color, humanPoints, this._TotalDistance, this._GameScene);         
                              
-                this._PrevPosX = this._CurrPosX;
-                break;                
-            }
-            case 1:
-            case 2:
-        }
+                this._PrevPosX = this._CurrPosX;                               
+            
     }
     public generateColor()
     {
         let roll:number[]=[0,0,0];
-        roll[0] = Math.random()*2;
-        roll[1] = Math.random()*2;
-        roll[2] = Math.random()*2;
-        for(let i; i<3; i++)
+        roll[0] = Math.round(Math.random()*2);
+        roll[1] = Math.round(Math.random()*2);
+        roll[2] = Math.round(Math.random()*2);
+        for(let i=0; i<3; i++)
         {
             if(roll[i]==0)roll[i]=0;
             else if(roll[i]==1)roll[i]=128;
             else if(roll[i]==2)roll[i]=255;
         }
+        
         return Engineer.Color.FromRGBA(roll[0], roll[1], roll[2], 255);
     }
 }
