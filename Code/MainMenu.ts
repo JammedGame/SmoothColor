4c0be2ed4aba2493e7c84617ee9efb79f7d4da6f
Unknown <manojlovicsmilos@hotmail.com>
2017-12-24 17:05:32 +0100

    Added Title

--- Code/MainMenu.ts
+++ Code/MainMenu.ts
@@ -18,6 +18,7 @@ class MainMenu extends Engineer.Scene2D
     public Init() : void
     {
         this.Name = "Menu";
+        this.BackColor = Engineer.Color.White;
         this.GenerateBackground();
         let Buttons:any = new Engineer.TileCollection(null, ["Resources/Textures/Play.png", "Resources/Textures/Level.png"]);
         let Play:any = new Engineer.Tile();
@@ -25,7 +26,7 @@ class MainMenu extends Engineer.Scene2D
         Play.Collection = Buttons;
         Play.Index = 0;
         Play.Trans.Scale = new Engineer.Vertex(300, 200, 1);
-        Play.Trans.Translation = new Engineer.Vertex(300, 550, 0);
+        Play.Trans.Translation = new Engineer.Vertex(300, 600, 0);
         Play.Events.MouseDown.push(this.PlayClick.bind(this));
         this.AddSceneObject(Play);
         let Level:any = new Engineer.Tile();
@@ -33,7 +34,7 @@ class MainMenu extends Engineer.Scene2D
         Level.Collection = Buttons;
         Level.Index = 1;
         Level.Trans.Scale = new Engineer.Vertex(300, 200, 1);
-        Level.Trans.Translation = new Engineer.Vertex(1600, 550, 0);
+        Level.Trans.Translation = new Engineer.Vertex(1600, 600, 0);
         Level.Events.MouseDown.push(this.LevelClick.bind(this));
         this.AddSceneObject(Level);
         this._Game.AddScene(this);
@@ -51,13 +52,20 @@ class MainMenu extends Engineer.Scene2D
     }
     private GenerateBackground() : void
     {
-        let Backs:Engineer.TileCollection = new Engineer.TileCollection(null, ["Resources/Textures/cover.png"]);
+        let Backs:Engineer.TileCollection = new Engineer.TileCollection(null, ["Resources/Textures/cover.png", "Resources/Textures/Title.png"]);
         let Back:Engineer.Tile = new Engineer.Tile();
         Back.Name = "Back";
         Back.Collection = Backs;
         Back.Index = 0;
         Back.Trans.Scale = new Engineer.Vertex(1920, 1080, 1);
-        Back.Trans.Translation = new Engineer.Vertex(960, 540, 0);
+        Back.Trans.Translation = new Engineer.Vertex(960, 600, 0);
+        let Title:Engineer.Tile = new Engineer.Tile();
+        Title.Name = "Title";
+        Title.Collection = Backs;
+        Title.Index = 1;
+        Title.Trans.Scale = new Engineer.Vertex(1920, 1080, 1);
+        Title.Trans.Translation = new Engineer.Vertex(960, 500, 0);
         this.AddSceneObject(Back);
+        this.AddSceneObject(Title);
     }
 }
